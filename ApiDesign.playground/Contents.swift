9ebbe3247e1fd02913424bc89f61ace13f2f0577
seyhagithub <hiemseyha168@gmail.com>
2018-03-17 14:37:45 +0700

    real example

--- ApiDesign.playground/Contents.swift
+++ ApiDesign.playground/Contents.swift
@@ -56,22 +56,58 @@ fileprivate enum EndPointConfiguration {
 // EndPoint Manager
 
 struct EndPoint: EndPointConfigurationActiveType {
-   
    fileprivate typealias me = EndPoint
-   
    fileprivate static var active: String!
-   
+}
+
+
+// Post Directory
+extension EndPoint {
    public enum Post: String {
-      case getPost
+      case get
+      case update
+      case delete
       
-      public var value: String {
+      var value: String {
          switch self {
-         case .getPost:
+         case .get:
             return me.active + "posts"
+         case .delete:
+            return me.active + "delete"
+         case .update:
+            return me.active + "update"
          }
       }
    }
 }
+
+// User Directory
+extension EndPoint {
+   
+   public enum User: String {
+      case login
+      case logout
+      case update
+      case delete
+      case get
+      
+      var value: String {
+         switch self {
+         case .login:
+            return me.active + "login"
+         case .logout:
+            return me.active + "logout"
+         case .update:
+            return me.active + "update"
+         case .delete:
+            return me.active + "delete"
+         case .get:
+            return me.active + "get"
+         }
+      }
+   }
+}
+
 
 
 // Configuration
@@ -80,6 +116,17 @@ EndPoint.active = EndPointConfiguration.production.active
 
 
 
-// Usage
-EndPoint.Post.getPost.value
+// Usage of post
+EndPoint.Post.delete.value
+EndPoint.Post.get.value
+EndPoint.Post.update.value
+
+
+
+// Usage of user
+EndPoint.User.login.value
+EndPoint.User.logout.value
+EndPoint.User.delete.value
+EndPoint.User.update.value
+EndPoint.User.get.value
 
